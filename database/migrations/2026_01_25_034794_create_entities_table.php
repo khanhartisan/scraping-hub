75ff4922382d832522f7d27f9a28c8380eb812cf
khanh.artisan <khanh.artisan@gmail.com>
2026-01-27 11:41:23 +0700

    Add `source_id` field, timestamps, and an index to the `entities` table in migration

--- database/migrations/2026_01_25_034794_create_entities_table.php
+++ database/migrations/2026_01_25_034794_create_entities_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('entities', function (Blueprint $table) {
             $table->ulid('id')->primary();
+            $table->ulid('source_id');
             $table->unsignedTinyInteger('type')->default(\App\Enums\EntityType::UNCLASSIFIED->value);
             $table->unsignedTinyInteger('scraping_status')->default(\App\Enums\ScrapingStatus::PENDING->value);
 
@@ -28,6 +29,12 @@ return new class extends Migration
             $table->unsignedInteger('snapshots_count')->default(0);
 
             $table->timestamps();
+            $table->dateTime('source_published_at')->nullable();
+            $table->dateTime('source_updated_at')->nullable();
+            $table->dateTime('fetched_at')->nullable();
+
+            // Indexes
+            $table->index(['source_id', 'type', 'source_published_at'], 'source_index');
         });
     }
 
